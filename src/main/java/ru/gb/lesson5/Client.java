a7d156969fe3579087d3ecbb0f0ed1cb5bae3ef2
Pavel <Technic47@gmail.com>
2023-12-27 19:54:20 +0300

    HomeWork

--- src/main/java/ru/gb/lesson5/Client.java
+++ src/main/java/ru/gb/lesson5/Client.java
@@ -8,37 +8,37 @@ import java.util.Scanner;
 
 public class Client {
 
-  public static void main(String[] args) throws IOException {
-    final Socket client = new Socket("localhost", Server.PORT);
-    // чтение
-    new Thread(() -> {
-      try (Scanner input = new Scanner(client.getInputStream())) {
-        while (true) {
-          System.out.println(input.nextLine());
-        }
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }).start();
+    public static void main(String[] args) throws IOException {
+        final Socket client = new Socket("localhost", Server.PORT);
+        // чтение
+        new Thread(() -> {
+            try (Scanner input = new Scanner(client.getInputStream())) {
+                while (true) {
+                    System.out.println(input.nextLine());
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
-    // запись
-    new Thread(() -> {
-      try (PrintWriter output = new PrintWriter(client.getOutputStream(), true)) {
-        Scanner consoleScanner = new Scanner(System.in);
-        while (true) {
-          String consoleInput = consoleScanner.nextLine();
-          output.println(consoleInput);
-          if (Objects.equals("q", consoleInput)) {
-            client.close();
-            break;
-          }
-        }
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    }).start();
+        // запись
+        new Thread(() -> {
+            try (PrintWriter output = new PrintWriter(client.getOutputStream(), true)) {
+                Scanner consoleScanner = new Scanner(System.in);
+                while (true) {
+                    String consoleInput = consoleScanner.nextLine();
+                    output.println(consoleInput);
+                    if (Objects.equals("q", consoleInput)) {
+                        client.close();
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
-  }
+    }
 }
 
 
--- src/main/java/ru/gb/lesson5/Server.java
+++ src/main/java/ru/gb/lesson5/Server.java
@@ -13,75 +13,84 @@ import java.util.Scanner;
 
 public class Server {
 
-  // message broker (kafka, redis, rabbitmq, ...)
-  // client sent letter to broker
+    // message broker (kafka, redis, rabbitmq, ...)
+    // client sent letter to broker
 
-  // server sent to SMTP-server
+    // server sent to SMTP-server
 
-  public static final int PORT = 8181;
+    public static final int PORT = 8181;
 
-  private static long clientIdCounter = 1L;
-  private static Map<Long, SocketWrapper> clients = new HashMap<>();
+    private static long clientIdCounter = 1L;
+    private static Map<Long, SocketWrapper> clients = new HashMap<>();
 
-  public static void main(String[] args) throws IOException {
-    try (ServerSocket server = new ServerSocket(PORT)) {
-      System.out.println("Сервер запущен на порту " + PORT);
-      while (true) {
-        final Socket client = server.accept();
-        final long clientId = clientIdCounter++;
-
-        SocketWrapper wrapper = new SocketWrapper(clientId, client);
-        System.out.println("Подключился новый клиент[" + wrapper + "]");
-        clients.put(clientId, wrapper);
-
-        new Thread(() -> {
-          try (Scanner input = wrapper.getInput(); PrintWriter output = wrapper.getOutput()) {
-            output.println("Подключение успешно. Список всех клиентов: " + clients);
-
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущен на порту " + PORT);
             while (true) {
-              String clientInput = input.nextLine();
-              if (Objects.equals("q", clientInput)) {
-                // todo разослать это сообщение всем остальным клиентам
-                clients.remove(clientId);
-                clients.values().forEach(it -> it.getOutput().println("Клиент[" + clientId + "] отключился"));
-                break;
-              }
+                final Socket client = server.accept();
+                final long clientId = clientIdCounter++;
 
-              // формат сообщения: "цифра сообщение"
-              long destinationId = Long.parseLong(clientInput.substring(0, 1));
-              SocketWrapper destination = clients.get(destinationId);
-              destination.getOutput().println(clientInput);
+                SocketWrapper wrapper = new SocketWrapper(clientId, client);
+                System.out.println("Подключился новый клиент[" + wrapper + "]");
+                clients.put(clientId, wrapper);
+
+                new Thread(() -> {
+                    try (Scanner input = wrapper.getInput(); PrintWriter output = wrapper.getOutput()) {
+                        output.println("Подключение успешно. Список всех клиентов: " + clients);
+
+                        while (true) {
+                            String clientInput = input.nextLine();
+                            char firstChar = clientInput.charAt(0);
+                            switch (firstChar) {
+                                case '@' -> {
+                                    try {
+                                        int number = Integer.parseInt(String.valueOf(firstChar));
+                                        String message = clientInput.substring(2);
+
+                                    } catch (NumberFormatException e) {
+
+                                    }
+                                }
+                                case 'q' -> {
+                                    clients.remove(clientId);
+                                    clients.values().forEach(it -> it.getOutput().println("Клиент[" + clientId + "] отключился"));
+                                }
+                            }
+                            // формат сообщения: "цифра сообщение"
+                            long destinationId = Long.parseLong(clientInput.substring(0, 1));
+                            SocketWrapper destination = clients.get(destinationId);
+                            destination.getOutput().println(clientInput);
+                        }
+                    }
+                }).start();
             }
-          }
-        }).start();
-      }
+        }
     }
-  }
 
 }
 
 @Getter
 class SocketWrapper implements AutoCloseable {
 
-  private final long id;
-  private final Socket socket;
-  private final Scanner input;
-  private final PrintWriter output;
+    private final long id;
+    private final Socket socket;
+    private final Scanner input;
+    private final PrintWriter output;
 
-  SocketWrapper(long id, Socket socket) throws IOException {
-    this.id = id;
-    this.socket = socket;
-    this.input = new Scanner(socket.getInputStream());
-    this.output = new PrintWriter(socket.getOutputStream(), true);
-  }
+    SocketWrapper(long id, Socket socket) throws IOException {
+        this.id = id;
+        this.socket = socket;
+        this.input = new Scanner(socket.getInputStream());
+        this.output = new PrintWriter(socket.getOutputStream(), true);
+    }
 
-  @Override
-  public void close() throws Exception {
-    socket.close();
-  }
+    @Override
+    public void close() throws Exception {
+        socket.close();
+    }
 
-  @Override
-  public String toString() {
-    return String.format("%s", socket.getInetAddress().toString());
-  }
+    @Override
+    public String toString() {
+        return String.format("%s", socket.getInetAddress().toString());
+    }
 }
